f34064defa5efa866f4f49308b36964655581bab
mrglox <contact@morgan-leroux.com>
2021-11-12 10:42:54 +0100

    add handlebars

--- public/app.js
+++ public/app.js
@@ -1,1 +1,36 @@
-console.log(test);
+console.log("test");
+
+const socket = io();
+
+const form = document.forms[0];
+const input = form.querySelector("input");
+
+const list = document.querySelector("ul");
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  console.log(input.value);
+
+  socket.emit("message", input.value);
+  input.value = "";
+});
+
+socket.on("message", (msg) => {
+  const li = document.createElement("li");
+  li.textContent = msg;
+  list.appendChild(li);
+});
+
+socket.on("user connected", (msg) => {
+  const li = document.createElement("li");
+  li.textContent = `*${msg}*`;
+  li.classList.add("newUser");
+  list.appendChild(li);
+});
+
+socket.on("user disconnected", (msg) => {
+  const li = document.createElement("li");
+  li.textContent = `*${msg}*`;
+  li.classList.add("newUser");
+  list.appendChild(li);
+});
--- server.js
+++ server.js
@@ -1,18 +1,62 @@
-const express = require("express");
-const path = require("path");
+// NODE dependencies
+// const path = require("path");
+const http = require("http");
 
+// PROJECT dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+// init const
 const app = express();
 const port = 3000;
 
-const http = require("http");
 const server = http.createServer(app);
 
-app.get("/", (request, response) => {
-  response.sendFile(path.resolve(__dirname + "/index.html"));
-});
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+/**
+ * INIT VIEW ENGINE
+ */
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+/**
+ * EXPOSE FILES
+ */
 app.use(express.static("public"));
 
+/**
+ * HANDLE ROUTES
+ */
+app.get("/", (request, response) => response.render("home"));
+app.get("/signin", (request, response) => response.render("signin"));
+app.get("/signup", (request, response) => response.render("signup"));
+
+/**
+ * SOCKET SYSTEM
+ */
+io.on("connection", (socket) => {
+  //   console.log(socket);
+  console.log("user connected");
+
+  io.emit("user connected", "user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+
+    io.emit("user disconnected", "user disconnected");
+  });
+
+  socket.on("message", (msg) => {
+    console.log("message", msg);
+    io.emit("message", msg);
+  });
+});
+
+/**
+ * SERVER INIT
+ */
 server.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
